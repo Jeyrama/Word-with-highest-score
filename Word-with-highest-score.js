fcf32d1ed842620c006996f146e8c7b8b2534a4b
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-27 15:37:08 -0700

    Update Word-with-highest-score.js

--- Word-with-highest-score.js
+++ Word-with-highest-score.js
@@ -39,3 +39,16 @@ function high(x){
 }
 
 // or
+
+function high(words) {
+
+  const alpha = ' abcdefghijklmnopqrstuvwxyz';
+  const score = word => word.split('').reduce((a, b) => a + alpha.indexOf(b), 0);
+
+  return words
+    .split(' ')
+    .map((word, pos) => ({ word: word, pos: pos, score: score(word) }))
+    .sort((a, b) => a.score - b.score || b.pos - a.pos)
+    .pop()
+    .word;
+}
